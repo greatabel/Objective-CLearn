563fae7fb705bc6bb2c4d82d09afd668d4b94c6a
greatabel <greatabel1@126.com>
2015-03-03 23:20:56 +0800

    stringreadfromfile

--- Objective-C Quick Syntax Reference/HelloWorld/HelloWorld/part2.m
+++ Objective-C Quick Syntax Reference/HelloWorld/HelloWorld/part2.m
@@ -11,7 +11,39 @@
 @implementation part2
 
 -(void)someMethod{
-    NSLog(@"test in someMethod");
+    NSLog(@"\n\n\n test in someMethod:NSString");
+    
+    NSString *firstName = @"great";
+    NSString *lastName = @"abel";
+    
+    NSString *name = [NSString stringWithFormat:@"%@ %@",firstName,lastName];
+    NSLog(@"name: %@",name);
+    
+    NSString *fileName = @"/Users/abel/Downloads/AbelProject/Objective-CLearn/Objective-C Quick Syntax Reference/HelloWorld/HelloWorld/test";
+    NSString *fileContents = [NSString
+                              stringWithContentsOfFile:fileName encoding:NSStringEncodingConversionAllowLossy error:nil];
+    NSLog(@"fileContents: %@",fileContents);
+    
+    NSLog(@"NSMutableString:");
+    NSMutableString *alpha = [NSMutableString stringWithString:@"A Test"];
+    [alpha insertString:@"B" atIndex:[alpha length]];
+    NSLog(@"NSMutableString:%@",alpha);
+    
+    [alpha appendString:@"CEnd"];
+    
+    NSRange range;
+    range.location = 1;
+    range.length = 5;
+    [alpha deleteCharactersInRange:range];
+    NSLog(@"NSMutableString 1:%@",alpha);
+    
+    //find and replace
+    range.location = 0;
+    range.length = 2;
+    [alpha replaceOccurrencesOfString:@"AB" withString:@"WWW" options:NSLiteralSearch range:range];
+    NSLog(@"NSMutableString 2:%@",alpha);
+    
+        NSLog(@"\n\n\n test in someMethod:NSNumber");
     
 }
 @end
