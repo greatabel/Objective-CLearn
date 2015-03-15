f9a46e691e14992b832c886b9725891ad876d0d7
greatabel <greatabel1@126.com>
2015-03-15 14:51:39 +0800

    literal is prefered

---
+++ Effective Objective-C2.0/3PreferLiteralSyntax/3PreferLiteralSyntax/main.m
@@ -0,0 +1,80 @@
+//
+//  main.m
+//  3PreferLiteralSyntax
+//
+//  Created by abel on 15/3/15.
+//  Copyright (c) 2015年 abel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       NSString *someString = @"Effective Objective-C 2.0";
+        NSNumber *someNumber = @111;
+        NSNumber *floatNumber = @2.22222f;
+        NSNumber *doubleNubmer =@3.1415926;
+        NSNumber *boolNumber = @YES;
+        NSNumber *charNumber = @'a';
+        
+        int  x = 5;
+        float y = 6.6;
+        NSNumber *expressionNumber = @(x * y);
+        
+        //before literals
+        NSArray *animals =
+        [NSArray arrayWithObjects:@"cat",@"dog",@"mouse", nil];
+        NSLog(@"animals= %@",animals);
+        
+        NSArray *newAnimals = @[@"cat",@"dog",@"bager"];
+        NSLog(@"newAnimals= %@",newAnimals);
+        
+//        NSArray *newAnimalsWithNil = @[@"cat",@"dog",@"bager",nil];
+//        NSLog(@"newAnimalsWithNil= %@",newAnimalsWithNil);
+        
+        //before literals
+        NSArray *dog = [animals objectAtIndex:1];
+        //now
+        NSString *newdog = animals[1];
+        
+        //before
+        NSDictionary *personData =
+        [NSDictionary dictionaryWithObjectsAndKeys:@"great",@"fristName",@"abel"
+         ,@"lastName",[NSNumber numberWithInt:29],@"age"];
+        
+        //now
+        NSDictionary *newpersonData =
+        @{
+          @"firstName":@"great",
+          @"lastName":@"abel",
+          @"age":@28
+          };
+        
+        
+        //before
+        NSString *lastName =[personData objectForKey:@"lastName"];
+        //now
+        NSString *newLastName = personData[@"lastName"];
+        
+        //before
+        NSMutableArray *mutableArray = [NSMutableArray arrayWithObjects:@"one", @"two", @"three", nil];
+        [mutableArray replaceObjectAtIndex:1 withObject:@"dog"];
+        
+        //now
+        mutableArray[1]=@"dog";
+        
+        NSMutableArray *mutable =[@[@1,@2,@3,@"test",@"test1"] mutableCopy];
+        NSLog(@"test:%@",mutable);
+        [mutable addObject:@"对象1"];
+        [mutable addObject:@"对象2"];
+        [mutable addObject:@"对象3"];
+        [mutable addObject:@"对象4"];
+        [mutable insertObject:@"搅局的" atIndex:2];
+        [mutable addObject:@"test2"];
+        NSLog(@"test1:%@",mutable);
+        NSLog(@"汉字%@",[mutable objectAtIndex:6]);
+        
+        
+    }
+    return 0;
+}
