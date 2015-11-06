61062282d4a1f67802aea0a8b168825370371229
greatabel <greatabel1@126.com>
2015-11-06 15:12:54 +0800

    update

---
+++ p2pChatDemo/MCDemo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  MCDemo
+//
+//  Created by Gabriel Theodoropoulos on 1/6/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MCManager.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong) MCManager *mcManager;
+
+@end
---
+++ p2pChatDemo/MCDemo/FirstViewController.h
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.h
+//  MCDemo
+//
+//  Created by Gabriel Theodoropoulos on 1/6/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *txtMessage;
+@property (weak, nonatomic) IBOutlet UITextView *tvChat;
+
+
+- (IBAction)sendMessage:(id)sender;
+- (IBAction)cancelMessage:(id)sender;
+
+
+@end
