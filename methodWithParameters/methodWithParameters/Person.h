fb454d2cb743d8d063bf2bf885a2fb53c4dcb5d7
greatabel <greatabel1@126.com>
2015-12-10 17:18:22 +0800

    add person

---
+++ methodWithParameters/methodWithParameters/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  methodWithParameters
+//
+//  Created by 万畅 on 15/12/10.
+//  Copyright © 2015年 abelwan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ methodWithParameters/methodWithParameters/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  methodWithParameters
+//
+//  Created by 万畅 on 15/12/10.
+//  Copyright © 2015年 abelwan. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
